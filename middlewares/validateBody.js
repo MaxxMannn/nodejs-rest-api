2a9e91abd4d4ffe5a1a73708748cbcefb26a0a04
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-09-15 14:37:45 +0200

    fixfavorite

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,7 +3,7 @@ const HttpError = require("../helpers/HttpError");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
        if (Object.keys(req.body).length === 0) {
-       return next(HttpError(400, 'missing fields favorite'));
+       return next(HttpError(400, 'missing fields'));
     }
 
     const { error } = schema.validate(req.body);
